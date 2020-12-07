fa2fad5ede6d6162ce3e08a12207a7d3836f4d41
dorukErgun <doruk.ergun2017@gtu.edu.tr>
2020-12-08 01:34:03 +0300

    Added facility picker page, Created a server to test that page.

---
+++ ejs_trial_server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const facilityData = require('./data.json');
+console.log(facilityData);
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req, res) => {
+    console.log("HI");
+})
+
+app.get('/:facilitytype', (req, res) => {
+    const { facilitytype } = req.params;
+    const data = facilityData[facilitytype];
+    console.log(data);
+    if(data){
+        res.render('facilitypicker', { ...data });
+    } else {
+        res.render('notfound', {facilitytype});
+    }
+})
+
+app.get('/:facilitytype/:facility', (req, res) => {
+    const { facility } = req.params;
+    const data = facilityData[facility];
+    console.log("sad");
+    if(data){
+        res.render('facility', { ...data });
+    } else {
+        res.render('notfound', { facility });
+    }
+})
+
+app.listen(3000, () => {
+    console.log("LISTENING ON PORT 3000");
+})
